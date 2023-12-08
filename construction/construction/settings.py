55ee6850ad4cb172db31453601d93c002757ea39
Emmanuel1 <Nnamdiemmanuel01@gmail.com>
2023-12-09 00:45:42 +0100

    Update settings.py

--- construction/construction/settings.py
+++ construction/construction/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = 'static/'
 
