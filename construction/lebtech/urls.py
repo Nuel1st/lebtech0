43c023ca3e4167962509f215620dffe8bd22f71e
Emmanuel1 <Nnamdiemmanuel01@gmail.com>
2023-12-08 16:55:59 +0100

    oo

--- construction/lebtech/urls.py
+++ construction/lebtech/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 
 urlpatterns= [
-    path('',  views.home, name='home'),
-    path('about/', views.about, name='about' ),
-    path('contact/', views.contact, name='contact')
-    
+    path('',  views.home, name="home"),
+    path('about/', views.about, name="about" ),
+    path('contact/', views.contact, name="contact"),
+    path('service/', views.service, name="service"),
+    path('team/', views.team, name="team"),
+    path('project', views.project, name="project")
+
 ]
